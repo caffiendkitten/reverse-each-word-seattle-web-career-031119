117188921511f9695dbec9a0b9e475096277e6ba
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-03-04 01:02:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(string)
+  array = []
 
+  array.push(string.split(' '))
 
-
-    return string.reverse
-
+  array.each do |reverse |
+    return array.reverse
+  end
+  #return array
 end
